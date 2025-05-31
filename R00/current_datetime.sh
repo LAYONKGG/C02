b0cd2304447575e95efc557b1742ab7952c5802f
LAYONKGG <mlaouali374@gmail.com>
2025-05-31 12:17:15 +0100

    Delete R00 directory

--- R00/current_datetime.sh
+++
@@ -1,3 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-#Affiche la date et l'heure actuelle
-date "+%Y-%m-%d %H:%M:%S"
--- R00/list_files.sh
+++
@@ -1,3 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-#Listes des fichiers et repertoires du repertoire courant
-ls -la
